7bd1284dc4f6127a5813a79b47843885c73a4c3a
kendavidson <ken.j.davidson@live.ca>
2017-09-18 22:00:21 -0400

    Initial Commit

---
+++ src/Provider/YahooFantasy.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Kenjdavidson\OAuth\YahooFantasy\Provider;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use Psr\Http\Message\ResponseInterface;
+use League\OAuth2\Client\Token\AccessToken;
+
+/**
+ * YahooFantasySportsProvider implements Yahoo Fantasy Sports specific details
+ * using the League/oath2-client library.  The provider allows connectivity
+ * to most of the major Yahoo Fantasy resources.
+ *
+ * @author kendavidson
+ */
+class YahooFantasy extends AbstractProvider {       
+    
+    /**
+     * Checks a provider response for errors.
+     *
+     * @throws IdentityProviderException
+     * @param  ResponseInterface $response
+     * @param  array|string $data Parsed response data
+     * @return void
+     */
+    protected function checkResponse(ResponseInterface $response, $data) {
+        
+    }
+
+    /**
+     * Generates a resource owner object from a successful resource owner
+     * details request.
+     *
+     * @param  array $response
+     * @param  AccessToken $token
+     * @return ResourceOwnerInterface
+     */
+    protected function createResourceOwner(array $response, AccessToken $token) {
+        
+    }
+
+    /**
+     * Returns the default scopes used by this provider.
+     *
+     * This should only be the scopes that are required to request the details
+     * of the resource owner, rather than all the available scopes.
+     *
+     * @return array
+     */
+    protected function getDefaultScopes() {
+        
+    }
+
+    /**
+     * Returns the base URL for requesting an access token.
+     *
+     * Eg. https://oauth.service.com/token
+     *
+     * @param array $params
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params) {
+        
+    }
+
+    /**
+     * Returns the base URL for authorizing a client.
+     *
+     * Eg. https://oauth.service.com/authorize
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl() {
+        
+    }
+
+    /**
+     * Returns the URL for requesting the resource owner's details.
+     *
+     * @param AccessToken $token
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token) {
+        
+    }
+
+}
